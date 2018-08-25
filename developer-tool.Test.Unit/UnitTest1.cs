72d51f452c4190d95652fd7f8a58f30a9390491f
Nikola <nikolabojkovic@gmail.comm>
2018-08-26 01:12:33 +0200

    unist tes for getById added

--- developer-tool.Test.Unit/UnitTest1.cs
+++ developer-tool.Test.Unit/UnitTest1.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Linq.Expressions;
 using Xunit;
 using Moq;
 using WebApi.Interfaces;
@@ -11,7 +12,7 @@ namespace TestUnit
     public class UnitTest1
     {
         [Fact]
-        public void TestServiceGet_ShouldRegurnCount2()
+        public void TestServiceGet_AllData_ShouldRegurnCount2()
         {
             // Arrange
             var testArray = new Test[]
@@ -20,7 +21,7 @@ namespace TestUnit
                 new Test { FirstName = "Test 2" }
             };
             Mock<IRepository<Test>> testRepository = new Mock<IRepository<Test>>();
-            testRepository.Setup(x => x.FindAll()).Returns(testArray.AsQueryable ());
+            testRepository.Setup(x => x.FindAll()).Returns(testArray.AsQueryable());
             var testService = new TestService(testRepository.Object);
             
             // Act
@@ -28,6 +29,29 @@ namespace TestUnit
 
             // Assert
             Assert.Equal(2, results.Count());
+            testRepository.Verify(x => x.FindAll(), Times.Once);
+        }
+
+        [Theory]
+        [InlineData(1, "Test 1")]
+        [InlineData(2, "Test 2")]
+        public void TestServiceGet_ById_ShouldRegurnTestObject(int id, string firstName)
+        {
+            // Arrange
+            var test = new Test { Id = id, FirstName = firstName };
+            var testArray = new Test[] {
+                test
+            };
+            Mock<IRepository<Test>> testRepository = new Mock<IRepository<Test>>();
+            testRepository.Setup(repo => repo.Find(It.IsAny<Expression<Func<Test, bool>>>())).Returns(testArray.AsQueryable());
+            var testService = new TestService(testRepository.Object);
+            
+            // Act
+            var result = testService.GetById(id);
+      
+            // Assert
+            Assert.Equal(firstName, result.FirstName);
+            testRepository.Verify(x => x.Find(It.IsAny<Expression<Func<Test, bool>>>()), Times.Once);
         }
     }
 }
