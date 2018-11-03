b4416e6c17cf0e3ac39853deee081b6bfd7febb4
Nikola <nikolabojkovic@gmail.comm>
2018-11-03 19:07:40 +0100

    reminder active property private set added

--- developer-tool.Domain/Models/Reminder.cs
+++ developer-tool.Domain/Models/Reminder.cs
@@ -6,7 +6,7 @@ namespace Domain.Models
     public class Reminder
     {       
         public int Id { get; private set; }
-        public bool Active { get; set; }
+        public bool Active { get; private set; }
         public DateTime Time { get; private set; }
         public ReminderTimeOffset TimeOffset { get; private set; }
 
