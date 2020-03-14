3bf785470347bf7968062d37d87ecce09b9a8d9b
Nikola <nikolabojkovic@gmail.comm>
2020-03-14 22:25:25 +0100

    test environment fix

--- developer-tool.WebApi/Startup.cs
+++ developer-tool.WebApi/Startup.cs
@@ -116,7 +116,7 @@ namespace WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
+            else if (env.EnvironmentName.ToLower() == "production")
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
