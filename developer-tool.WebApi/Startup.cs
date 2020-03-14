bd1266ff0d2ce52d7d1dc94b16612aeef2fd27f7
Nikola <nikolabojkovic@gmail.comm>
2020-03-14 21:36:33 +0100

    do not redirect to https for development environment, cv file updated

--- developer-tool.WebApi/Startup.cs
+++ developer-tool.WebApi/Startup.cs
@@ -120,9 +120,8 @@ namespace WebApi
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            }
-
-            app.UseHttpsRedirection(); 
+                app.UseHttpsRedirection();
+            } 
 
             app.Use(async (HttpContext context, Func<Task> next) =>
             {
